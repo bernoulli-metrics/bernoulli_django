4b7a60c2179f7815463c8d1f0cff8bd62c33ea54
Joe Gasiorek <joe.gasiorek@gmail.com>
2014-02-23 08:21:58 -0800

    character off?

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='bernoulli_django',
-    version='0.1.3',
-    package=find_packages(),
+    version='0.1.4',
+    packages=find_packages(),
     license='MIT',
     description='Package to integrate django app with Bernoulli',
     url='https://github.com/bernoulli-metrics/bernoulli_django',

276217b9ea866e5ad17546e4ce53620dcd999b9d
Joe Gasiorek <joe.gasiorek@gmail.com>
2014-02-23 08:06:27 -0800

    update setup

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import os
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 # allow setup.py to be run from any path
@@ -7,12 +7,12 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='bernoulli_django',
-    version='0.1',
-    package=['bernoulli_django'],
+    version='0.1.1',
+    package=find_packages(),
     license='MIT',
     description='Package to integrate django app with Bernoulli',
     url='https://github.com/bernoulli-metrics/bernoulli_django',
     author='Joe Gasiorek',
     author_email='joe.gasiorek@gmail.com',
-    install_requires=['bernoulli']
+    install_requires=['bernoulli>=0.1.1']
 )

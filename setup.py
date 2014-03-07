5f52bc30960db253032ca4ef4ad0f9976f523f16
Joe Gasiorek <joe.gasiorek@gmail.com>
2014-03-06 19:54:02 -0800

    up version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='bernoulli_django',
-    version='0.1.7',
+    version='0.1.8',
     packages=find_packages(),
     license='MIT',
     description='Package to integrate django app with Bernoulli',
